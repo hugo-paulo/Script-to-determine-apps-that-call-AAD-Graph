3fc2e8d9952f358e900661ea77f7b68b3780d338
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2022-09-28 16:17:18 +0100

    Add files via upload

---
+++ Filter AAD Graph and print to screen.ps1
@@ -0,0 +1,12 @@
+﻿Connect-MgGraph -Scopes "Application.Read.All"
+
+Get-MgApplication -Filter "requiredResourceAccess/Any(x: x/resourceAppId eq '00000002-0000-0000-c000-000000000000')" -CountVariable appCount -ConsistencyLevel "eventual" -All -debug
+
+$AadGraphApps = Get-MgApplication -Filter "requiredResourceAccess/Any(x: x/resourceAppId eq '00000002-0000-0000-c000-000000000000')" -CountVariable appCount -ConsistencyLevel "eventual" -All | Format-List DisplayName, AppId, CreatedDateTime 
+
+
+# $AadGraphApps | Export-Csv -Path "C:\<path to file>\AADGraphAppList.csv" 
+
+$AadGraphApps | Out-File "C:\<path to file>\AADGraphAppList.csv"
+
+Disconnect-MgGraph
